22f90a7c1d70a0c2e8328ab81453225f1a4f4d35
Nikhil Prajapati <149403309+nikhil-prajapati-201@users.noreply.github.com>
2026-02-27 13:26:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// email message generater
+(function(){
+  emailjs.init("PmrBcSA2c0k8MzzOz"); 
+})();
+
+document.getElementById("contact-form")
+.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  emailjs.sendForm(
+    "service_n01kz32",
+    "template_7y7df3j",
+    this
+  ).then(function() {
+      alert("Message Sent Successfully");
+    }, function(error) {
+      alert("Failed to send");
+      console.log(error);
+    });
+});
